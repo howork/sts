7133a684d5a9563ab1e9156f134f70b4361e82ac
Munho Choi <howork@gmail.com>
2014-05-07 23:25:18 +0900

    initial commit
    create C project using NetBeans
    add LICENSE
    add short README

---
+++ README.md
@@ -0,0 +1,5 @@
+SmartTalk Script
+================
+
+The **SmartTalk Script** is a script language for easy use.
+
---
+++ main.c
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2014 Munho Choi
+ * 
+ * See the file LICENSE.txt or LICENSE-KOR.txt for copying permission.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+
+    printf("Hello, world!\n");
+    
+    return (EXIT_SUCCESS);
+}
+
